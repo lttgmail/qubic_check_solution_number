6b4c5e8b5e56f6c08a5d0a3b0eff15469caefde0
Taiting Liu <149050968+lttgmail@users.noreply.github.com>
2024-03-22 20:48:09 +0800

    Add files via upload

---
+++ check_solutions.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Find current lock file
+FILE=$(ls stats.*.lock | sort -t. -k2,2n | tail -n 1)
+FOUND_SOLUTIONS=$(jq '.FoundSolutions' "$FILE")
+FILE_PATH=$(pwd)
+EMAIL=$(jq -r '.email' "$FILE_PATH/config.json")
+NODE_NAME=$(jq -r '.alias' "$FILE_PATH/config.json")
+SAVE_FILE="$FILE_PATH/Solutions.txt"
+
+if [ ! -f "$SAVE_FILE" ]; then
+    echo "$FOUND_SOLUTIONS" > "$SAVE_FILE"
+fi
+
+old_value=$(cat "$SAVE_FILE")
+
+if [ "$FOUND_SOLUTIONS" -gt "$old_value" ]; then
+    echo "$FOUND_SOLUTIONS" > "$SAVE_FILE"
+    
+    echo "Found New Solutions, total solutions $FOUND_SOLUTIONS" | mail -s "$NODE_NAME" "$EMAIL"
+fi
